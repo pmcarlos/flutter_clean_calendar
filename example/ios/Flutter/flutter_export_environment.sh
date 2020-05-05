08ee7b29a0d82cc0db6b25678e14ba785383d167
pmcarlos <pmcarlosdev@gmail.com>
2020-05-05 09:18:21 -0500

    Fixed styling, fixed example

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/pmcarlos/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/pmcarlos/Documents/Development/Flutter/flutter_clean_calendar/example"
+export "FLUTTER_TARGET=/Users/pmcarlos/Documents/Development/Flutter/flutter_clean_calendar/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/pmcarlos/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
