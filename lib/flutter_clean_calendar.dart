6a9982ad9154e4ce003a2b1cd56b9bbd28ee6734
nairox <nairox@gmail.com>
2020-07-29 10:25:59 -0500

    Update flutter_clean_calendar.dart

--- lib/flutter_clean_calendar.dart
+++ lib/flutter_clean_calendar.dart
@@ -280,7 +280,7 @@ class _CalendarState extends State<Calendar> {
             children: <Widget>[
               SizedBox(width: 40.0),
               Text(
-                Utils.fullDayFormat(selectedDate),
+                DateFormat("EEEE MMMM dd, yyyy", widget.locale).format(_selectedDate),
                 style: widget.bottomBarTextStyle ?? TextStyle(fontSize: 13),
               ),
               IconButton(
