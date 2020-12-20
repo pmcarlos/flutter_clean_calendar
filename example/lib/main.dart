c55d89c6275b5706dee21e50ebaf25f666edacfa
Ralf Weinbrecher <developer@ralfweinbrecher.de>
2020-12-20 13:40:34 +0100

    #3 Added a dedicated class for clean calendar events
    This class allows it to set certain options to an event. Only the title (summary) is mandatory, all other parameters are optional. This is a breaking change.
    
    #3 Example adapted for usage of new events class
    #3 Example edited to make use of the CleanCalendarEvent class
    The example now imports the CleanCalendarEvent and uses the parameters of that class to show a more detailed event list for the selected day
    #3 The dots representing an event now take the color of the color property of the CleanCanlenderEvent
    If the CleanCalendarEvent has no color property set, you can set the color with the eventColor property of the CalendarTile
    #3 Example of how to use the color property of the CleanCalenderEvent
    #3 New property to set the string of the Today button on top of the screen
    By setting this property it is possible to set the caption of the Today button in local langauges
    #3 New property for callback function for changing to expanded view
    Eliminated double quotes and replaced them with single quotes
    #3 Example now forces the list of today's events being shown on load
    This is done by calling _handleNewDate in initState method

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'package:flutter_clean_calendar/clean_calendar_event.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,39 +32,42 @@ class _CalendarScreenState extends State<CalendarScreen> {
     print(_selectedEvents);
   }
 
-  List _selectedEvents;
+  List<CleanCalendarEvent> _selectedEvents;
   DateTime _selectedDay;
 
-  final Map<DateTime, List> _events = {
-    DateTime(2020, 5, 7): [
-      {'name': 'Event A', 'isDone': true},
+  final Map<DateTime, List<CleanCalendarEvent>> _events = {
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day): [
+      CleanCalendarEvent('Event A',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 12, 0),
+          description: 'A special event',
+          color: Colors.blue[700]),
     ],
-    DateTime(2020, 5, 9): [
-      {'name': 'Event A', 'isDone': true},
-      {'name': 'Event B', 'isDone': true},
-    ],
-    DateTime(2020, 5, 10): [
-      {'name': 'Event A', 'isDone': true},
-      {'name': 'Event B', 'isDone': true},
-    ],
-    DateTime(2020, 5, 13): [
-      {'name': 'Event A', 'isDone': true},
-      {'name': 'Event B', 'isDone': true},
-      {'name': 'Event C', 'isDone': false},
-    ],
-    DateTime(2020, 5, 25): [
-      {'name': 'Event A', 'isDone': true},
-      {'name': 'Event B', 'isDone': true},
-      {'name': 'Event C', 'isDone': false},
-    ],
-    DateTime(2020, 6, 6): [
-      {'name': 'Event A', 'isDone': false},
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 2):
+        [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
     ],
   };
 
   @override
   void initState() {
     super.initState();
+    // Force selection of today on first load, so that the list of today's events gets shown.
+    _handleNewDate(DateTime(
+        DateTime.now().year, DateTime.now().month, DateTime.now().day));
     _selectedEvents = _events[_selectedDay] ?? [];
   }
 
@@ -76,16 +81,19 @@ class _CalendarScreenState extends State<CalendarScreen> {
             Container(
               child: Calendar(
                 startOnMonday: true,
-                weekDays: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                weekDays: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
                 events: _events,
                 onRangeSelected: (range) =>
-                    print("Range is ${range.from}, ${range.to}"),
+                    print('Range is ${range.from}, ${range.to}'),
                 onDateSelected: (date) => _handleNewDate(date),
                 isExpandable: true,
                 eventDoneColor: Colors.green,
                 selectedColor: Colors.pink,
-                todayColor: Colors.yellow,
+                todayColor: Colors.blue,
                 eventColor: Colors.grey,
+                locale: 'de_DE',
+                todayButtonText: 'Heute',
+                expandableDateFormat: 'EEEE, dd. MMMM yyyy',
                 dayOfWeekStyle: TextStyle(
                     color: Colors.black,
                     fontWeight: FontWeight.w800,
@@ -99,21 +107,35 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
+  /// This function [_buildEventList] constructs the list of events of a selected day. This
+  /// list is rendered below the week view or the month view.
   Widget _buildEventList() {
     return Expanded(
       child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) => Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(width: 1.5, color: Colors.black12),
+        padding: EdgeInsets.all(0.0),
+        itemBuilder: (BuildContext context, int index) {
+          final CleanCalendarEvent event = _selectedEvents[index];
+          final String start =
+              DateFormat('HH:mm').format(event.startTime).toString();
+          final String end =
+              DateFormat('HH:mm').format(event.endTime).toString();
+          return ListTile(
+            contentPadding:
+                EdgeInsets.only(left: 2.0, right: 8.0, top: 2.0, bottom: 2.0),
+            leading: Container(
+              width: 10.0,
+              color: event.color,
             ),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 4.0),
-          child: ListTile(
-            title: Text(_selectedEvents[index]['name'].toString()),
+            title: Text(event.summary),
+            subtitle:
+                event.description.isNotEmpty ? Text(event.description) : null,
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text(start), Text(end)],
+            ),
             onTap: () {},
-          ),
-        ),
+          );
+        },
         itemCount: _selectedEvents.length,
       ),
     );
