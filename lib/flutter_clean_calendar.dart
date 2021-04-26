0eb716d70d52603076f869fef1fd1e8773635fb2
Ralf Weinbrecher <developer@ralfweinbrecher.de>
2021-04-26 11:10:04 +0200

    Fixes #36
    Corrected the calculation of the first day of week.

--- lib/flutter_clean_calendar.dart
+++ lib/flutter_clean_calendar.dart
@@ -532,10 +532,15 @@ class _CalendarState extends State<Calendar> {
   }
 
   _firstDayOfWeek(DateTime date) {
-    var day = new DateTime.utc(
+    var day = DateTime.utc(
         _selectedDate.year, _selectedDate.month, _selectedDate.day, 12);
-    return day.subtract(
-        new Duration(days: day.weekday - (widget.startOnMonday ? 1 : 0)));
+    if (widget.startOnMonday == true) {
+      day = day.subtract(Duration(days: day.weekday - 1));
+    } else {
+      // if the selected day is a Sunday, then it is already the first day of week
+      day = day.weekday == 7 ? day : day.subtract(Duration(days: day.weekday));
+    }
+    return day;
   }
 
   _lastDayOfWeek(DateTime date) {
