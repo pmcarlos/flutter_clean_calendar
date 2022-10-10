7f9617f2f8a2d1fbda8bd3e38a3a3db3c4aaa859
Toms <tomslaus@gmail.com>
2022-10-11 00:14:15 +0300

    color bugfix

--- lib/flutter_clean_calendar.dart
+++ lib/flutter_clean_calendar.dart
@@ -413,10 +413,10 @@ class _CalendarState extends State<Calendar> {
                         color: Colors.white,
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 2,
+                            blurRadius: 3,
+                            offset: Offset(0, 2), // changes position of shadow
                           ),
                         ],
                       ),
